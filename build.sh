283cca6dd037d366fd2802058fdc611c85ec1af8
Marek Czyz <mczyz@ra.rockwell.com>
2016-10-10 17:52:12 +0200

    Build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir -p thaliDontCheckIn/localdev
+cordova platform add android
 cd www/jxcore
 jx npm install ../../../Thali_CordovaPlugin/thali/ --save --no-optional --autoremove "*.gz"
 jx npm install --no-optional --autoremove "*.gz"
