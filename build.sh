b26b60140fd1f4aae21394482330ca940e610603
Marek Czyz <mczyz@ra.rockwell.com>
2016-10-10 17:41:45 +0200

    Build script

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 mkdir -p thaliDontCheckIn/localdev
+cd www/jxcore
+jx npm install ../../../Thali_CordovaPlugin/thali/ --save --no-optional --autoremove "*.gz"
+jx npm install --no-optional --autoremove "*.gz"
+find . -name "*.gz" -delete
