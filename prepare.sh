f73ae996224fc968e4ae457464bcf5358fbbde01
Andrew Aladjev <aladjev.andrew@gmail.com>
2016-11-03 17:53:12 +0300

    prepare cosmetics

--- prepare.sh
+++ prepare.sh
@@ -12,6 +12,9 @@ export JX_NPM_JXB
 mkdir -p thaliDontCheckIn/localdev
 cordova platform add android
 cd www/jxcore
-jx npm install ../../../Thali_CordovaPlugin/thali/ --save --no-optional --autoremove "*.gz"
+mkdir -p node_modules
+rm -f node_modules/thali
+ln -s ../../../../Thali_CordovaPlugin/thali node_modules/thali
+#jx npm install ../../../Thali_CordovaPlugin/thali/ --save --no-optional --autoremove "*.gz"
 jx npm install --no-optional --autoremove "*.gz"
 find . -name "*.gz" -delete
